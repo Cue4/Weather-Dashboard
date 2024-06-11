6342b6ce013f814946976c5db3e1b55649c2d356
Caliph Ferguson <Quasar@Caliphs-Mini.lan>
2024-06-11 08:51:33 -0400

    js update Andres

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,4 @@
-const searchElement = document.querySelector('#info-input');
+
 const submitEl = document.querySelector('#submit');
 const weatherInfoDiv = document.getElementById("weather-info");
 const apiKey = `7ff58370abe33390e3138197fc8cbbc9`;
@@ -9,11 +9,14 @@ function showResponse(event) {
   }
 
 submitEl.addEventListener("click", () => {
+    const searchElement = document.querySelector('#info-input').value;
     console.log("Search button clicked!");
+    console.log(searchElement);
+    getGeoData(searchElement)
 });
 
-function getWeatherData(city) {
-    const requestUrl = `https://openweathermap.org/data/2.5/weather?q=Columbus,Ohio&appid=${apiKey}`;
+function getGeoData(city) {
+    const requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
 
     fetch(requestUrl)
         .then(function (response){
@@ -21,25 +24,45 @@ function getWeatherData(city) {
         })
         .then(function (data) {
             console.log(data);
-            updateWeatherInfo(data);
-            updateSearchHistory(city);
+            console.log(data[0])
+
+        let lon = data[0].lon
+        let lat = data[0].lat
+        getWeatherData(lat, lon)
+            // updateWeatherInfo(data);
+            // updateSearchHistory(city);
         })
         .catch(function(error) {
             console.log(error);
         });
-}        
-getWeatherData()
+}      
+function getWeatherData(lat, lon){
+    const searchRequestUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
 
-const city = document.getElementById("weather-info");
-city.textContent = weatherInfoDiv.city;
-weatherInfoDiv.appendChild(searchElement);
+    fetch(searchRequestUrl)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
+    .catch(function (error){
+        console.log(error);
+    })
+}
+
+// getWeatherData()
+
+// const city = document.getElementById("weather-info");
+// city.textContent = weatherInfoDiv.city;
+// weatherInfoDiv.appendChild(searchElement);
 
 // Loop through the weather forecast data and create elements for each forecast
-weatherInfoDiv.(forecast => {
-    const forecastDiv = document.getElementsByClassName('box box-daily');
-    forecastDiv.textContent = `Date: ${forecast.dt_txt}, Temperature: ${forecast.main.temp}`;
-    weatherInfoDiv.appendChild(forecastDiv);
-});
+// weatherInfoDiv.(forecast => {
+//     const forecastDiv = document.getElementsByClassName('box box-daily');
+//     forecastDiv.textContent = `Date: ${forecast.dt_txt}, Temperature: ${forecast.main.temp}`;
+//     weatherInfoDiv.appendChild(forecastDiv);
+// });
 
 // function updateWeatherInfo(data) {
 //     weatherInfoDiv.innerHTML = `
@@ -66,23 +89,23 @@ weatherInfoDiv.(forecast => {
     
     
 
-    function updateSearchHistory(city) {
-        let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
-        searchHistory.push(city);
-        localStorage.setItem('searchHistory', JSON.stringify(searchHistory)); 
-        // updateSearchHistory();
-        displaySearchHistory();
-    }
+    // function updateSearchHistory(city) {
+    //     let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+    //     searchHistory.push(city);
+    //     localStorage.setItem('searchHistory', JSON.stringify(searchHistory)); 
+    //     // updateSearchHistory();
+    //     displaySearchHistory();
+    // }
 
-    function displaySearchHistory() {
-        const searchHistoryContainer = document.getElementById('search-history');
-        let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+    // function displaySearchHistory() {
+    //     const searchHistoryContainer = document.getElementById('search-history');
+    //     let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
         
-        // searchHistoryContainer.innerHTML = '<h2>RESULTS</h2>';
-        // searchHistoryContainer.innerHTML = '<h3>5-Day Forcast'
-        searchHistoryContainer.innerHTML += '<ul>' + searchHistory.map(city => `<li>${city}</li>`).join('') + '</ul>';
-    }
-    displaySearchHistory();
+    //     // searchHistoryContainer.innerHTML = '<h2>RESULTS</h2>';
+    //     // searchHistoryContainer.innerHTML = '<h3>5-Day Forcast'
+    //     searchHistoryContainer.innerHTML += '<ul>' + searchHistory.map(city => `<li>${city}</li>`).join('') + '</ul>';
+    // }
+    // displaySearchHistory();
 
     // submitEl.addEventListener("click", function (event) {
     //     event.preventDefault();
